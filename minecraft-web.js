f6ff22651745829fbbb8600dec9230a5b97720e2
Codie Newark <codienewark@outlook.com>
2024-05-17 16:51:40 +0100

    Update minecraft-web.js

--- minecraft-web.js
+++ minecraft-web.js
@@ -164,9 +164,9 @@ export default class MinecraftClient extends HTMLElement {
     this.#intro.style.display = 'none';
   
     this.#progress.style.display = 'unset';
-    const jarPath = "/files/client_1.2.5.jar"
+    const jarPath = "/files/client_b1.2_02.jar"
     await downloadFileToCheerpJ(
-      "https://piston-data.mojang.com/v1/objects/4a2fac7504182a97dcbcd7560c6392d7c8139928/client.jar",
+      "https://archive.org/download/Minecraft-JE-Beta/b1.2_02/b1.2_02.jar",
       jarPath,
       (downloadedBytes, totalBytes) => {
         this.#progress.value = downloadedBytes;
